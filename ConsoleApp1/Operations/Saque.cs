f86ce14de8e6049e0100868690fb8b38a4ccb554
Ronye do Lago Cornetti Rocha <ronye.rocha@sistemasseguros.local>
2018-02-23 15:40:30 -0300

    Class

---
+++ ConsoleApp1/Operations/Saque.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Saque
+{
+    public class Saque
+    {
+        public static bool IsValid(double value)
+        {
+            if (value >= 20 && value != 30 && value % 10 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
